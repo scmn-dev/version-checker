194241673b9b2d3bad19d919d6737363c0271583
abdfnx <abdfnabd@gmail.com>
2021-05-06 17:49:55 +0300

    update v_checker without verx

--- v_checker.go
+++ v_checker.go
@@ -9,6 +9,41 @@ import (
 	"github.com/briandowns/spinner"
 )
 
+check_w := `
+	$releases = "https://api.github.com/repos/secman-team/secman/releases"
+
+	$l = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+
+	$c = secman verx
+
+	if ($l -ne $c) {
+		$nr = "there's a new release of secman is avalaible: "
+		$up = "to upgrade run "
+		$smu = "sm-upg start"
+
+		Write-Host ""
+		Write-Host -NoNewline $nr -ForegroundColor DarkYellow
+		Write-Host "$c -> $l" -ForegroundColor DarkCyan
+		Write-Host -NoNewline $up -ForegroundColor DarkYellow
+		Write-Host $smu -ForegroundColor DarkCyan
+	}
+`
+
+check_ml := `
+	l=$(curl --silent "https://api.github.com/repos/secman-team/secman/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+	c=$(secman verx | tr -d '\n')
+
+	if [ $l != $c ]; then
+		nr="there's a new release of secman is avalaible:"
+		up="to upgrade run"
+		smu="secman upgrade"
+
+		echo ""
+		echo "$nr $c -> $l"
+		echo "$up $smu"
+	fi
+`
+
 func Checker() {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.Suffix = " 🔍 Checking for updates..."
@@ -17,9 +52,9 @@ func Checker() {
 	err, out, errout := shell.ShellOut("")
 	
 	if runtime.GOOS == "windows" {
-		err, out, errout = shell.PWSLOut("& ~/sm/vx.ps1 --sm")
+		err, out, errout = shell.PWSLOut(check_w)
 	} else {
-		err, out, errout = shell.ShellOut("verx --sm")
+		err, out, errout = shell.ShellOut(check_ml)
 	}
 		
 	if err != nil {
