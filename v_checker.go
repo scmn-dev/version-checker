4390506d67b2e9da6460c6c0cf00191bc560560f
abdfnx <abdfnabd@gmail.com>
2021-05-07 22:33:01 +0300

    move vars to function

--- v_checker.go
+++ v_checker.go
@@ -9,42 +9,43 @@ import (
 	"github.com/briandowns/spinner"
 )
 
-check_w := `
-	$releases = "https://api.github.com/repos/secman-team/secman/releases"
-
-	$l = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
-
-	$c = secman verx
-
-	if ($l -ne $c) {
-		$nr = "there's a new release of secman is avalaible: "
-		$up = "to upgrade run "
-		$smu = "sm-upg start"
-
-		Write-Host ""
-		Write-Host -NoNewline $nr -ForegroundColor DarkYellow
-		Write-Host "$c -> $l" -ForegroundColor DarkCyan
-		Write-Host -NoNewline $up -ForegroundColor DarkYellow
-		Write-Host $smu -ForegroundColor DarkCyan
-	}
-`
-
-check_ml := `
-	l=$(curl --silent "https://api.github.com/repos/secman-team/secman/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-	c=$(secman verx | tr -d '\n')
-
-	if [ $l != $c ]; then
-		nr="there's a new release of secman is avalaible:"
-		up="to upgrade run"
-		smu="secman upgrade"
-
-		echo ""
-		echo "$nr $c -> $l"
-		echo "$up $smu"
-	fi
-`
 
 func Checker() {
+	check_w := `
+		$releases = "https://api.github.com/repos/secman-team/secman/releases"
+	
+		$l = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+	
+		$c = secman verx
+	
+		if ($l -ne $c) {
+			$nr = "there's a new release of secman is avalaible: "
+			$up = "to upgrade run "
+			$smu = "sm-upg start"
+	
+			Write-Host ""
+			Write-Host -NoNewline $nr -ForegroundColor DarkYellow
+			Write-Host "$c -> $l" -ForegroundColor DarkCyan
+			Write-Host -NoNewline $up -ForegroundColor DarkYellow
+			Write-Host $smu -ForegroundColor DarkCyan
+		}
+	`
+	
+	check_ml := `
+		l=$(curl --silent "https://api.github.com/repos/secman-team/secman/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+		c=$(secman verx | tr -d '\n')
+	
+		if [ $l != $c ]; then
+			nr="there's a new release of secman is avalaible:"
+			up="to upgrade run"
+			smu="secman upgrade"
+	
+			echo ""
+			echo "$nr $c -> $l"
+			echo "$up $smu"
+		fi
+	`
+
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.Suffix = " 🔍 Checking for updates..."
 	s.Start()
